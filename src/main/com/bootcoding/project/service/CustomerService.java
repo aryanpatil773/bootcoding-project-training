5b18fa85afa8aad8455ff7062b770c23a3472103
aryanpatil773 <aryan.patil773@gmail.com>
2023-01-21 11:59:40 +0530

    java project training - updated customer service

--- src/main/com/bootcoding/project/service/CustomerService.java
+++ src/main/com/bootcoding/project/service/CustomerService.java
@@ -3,6 +3,9 @@ package com.bootcoding.project.service;
 import com.bootcoding.project.model.Customer;
 import com.bootcoding.project.utils.*;
 
+import java.util.ArrayList;
+import java.util.List;
+
 public class CustomerService {
     public Customer createCustomer(){
         Customer customer=new Customer();
@@ -11,9 +14,18 @@ public class CustomerService {
         customer.setPhoneNumber(PhoneNnumberGenerator.getPhone());
         customer.setEmailId(EmailIdGenerator.getEmailId(customer.getName()));
         customer.setDelivaryAddress(AddressGenerator.getName());
+
         return customer;
 
+    }
 
+    public List<Customer> getAllCustomers(){
+        List<Customer> customers = new ArrayList<>();
+        for(int i = 0; i < 50; i++){
+            Customer customer = createCustomer();
+            customers.add(customer);
+        }
+        return customers;
     }
     public void print(Customer customer){
         System.out.println("Customer Name:" + customer.getName());
